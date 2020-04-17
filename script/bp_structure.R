755c747847e5cfcb6acb1394004ab12f5248afe3
unknown <AsyaKarpovaa@gmail.com>
2020-04-17 21:50:08 +0300

    data overlaod :(

--- script/bp_structure.R
+++ script/bp_structure.R
@@ -1,16 +1,21 @@
 library(tidyverse)
+library(lubridate)
 library(rio)
-
+glimpse(plot_spread)
 for_plot = import('data/structure.csv')
-plot_spread = gather(for_plot, key = 'date', value = 'Value', `01.01.2000`:`01.04.2019`)
+ 
+plot_spread = gather(for_plot, key = 'date', value = 'Value', `01.01.2000`:`01.04.2019`)%>% select(V1, Value, date)
 plot_spread$date = parse_date_time(plot_spread$date, orders = 'dmY')
-plot_spread$V1 %>% unique()
-plot_spread$V1 %>% unique()
+plot_spread %>% select(V1) %>% unique()
+
+fuels = plot_spread %>% pivot_wider(names_from = `V1`, values_from = Value) %>% mutate(fuels = r_exp_op + r_exp_oil + r_exp_gas)
+
+fuels %>% group_by(date) %>% mutate(share = 1000 * fuels/`Current Account`) %>% select(date, share, `Current Account`, fuels) 
 
 structure = filter(plot_spread,
                    V1 != 'Current Account', V1 != 'Financial Account', V1 != 'Capital Account', V1 != 'Balance on Rent',
                    V1 != 'Financial Account', V1 != 'Net Errors and Omissions', V1 != 'Reserves and Related Items')
-structure
+
 
 ggplot(structure, aes(x = date, y = Value)) + 
   geom_line(color = 'red') + facet_wrap(.~V1) + xlab('') +ggtitle('Components of the Current Account')
@@ -25,11 +30,10 @@ cap_acc = filter(plot_spread, V1 == 'Capital Account') %>%
   ungroup()
 
 
-cur_acc$date = parse_date_time(cur_acc$date, orders = 'dmY')
-fin_acc$date = parse_date_time(fin_acc$date, orders = 'dmY')
-cap_acc$date = parse_date_time(cap_acc$date, orders = 'dmY')
+
 
 ggplot(cur_acc, aes(y = Value, x = date)) + geom_line(colour = 'red', size=0.5) + xlab('') + ggtitle('Current account')
 ggplot(fin_acc, aes(y = Value, x = date)) + geom_line(colour = 'red', size=0.5) + xlab('') + ggtitle('Financial account')
 ggplot(cap_acc, aes(y = Value, x = date)) + geom_line(colour = 'red', size=0.5) + xlab('') + ggtitle('Capital account')
 ggplot(fin_acc, aes(y = Value, x = date)) + geom_line(colour = 'red', size=0.5) + xlab('') + ggtitle('Current account') 
+
